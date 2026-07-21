6e9a8eeaab0cb5a091fef6bfb9cbb90c4ef5ece4
Thomas Wester <thomas_w@live.com>
2026-07-21 16:46:48 -0500

    Optimize caching database sync with task callbacks and register_future

--- sbn_parsl/components.py
+++ sbn_parsl/components.py
@@ -404,7 +404,7 @@ def larsoft_runfunc(
     )
 
     _transfer_ids(self, future.outputs[0])
-    executor.futures.add(future.outputs[0])
+    executor.register_future(future.outputs[0])
     executor._stage_counter += 1
 
     return StageResult(outputs=future.outputs)
